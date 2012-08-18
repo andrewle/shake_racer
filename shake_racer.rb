87020aa58de32b4eedd8b6698d610601c7073f39
Andrew Le <andrew@andrewdle.com>
2012-08-17 22:31:09 -0700

    Serve a static page from Goliath

---
+++ shake_racer.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "goliath"
+require 'goliath/websocket'
+
+class ShakeRacer < Goliath::WebSocket
+  use(Rack::Static,
+      :root => Goliath::Application.app_path("public"),
+      :urls => ['/index.html', '/css', '/js', '/img'])
+
+  def on_open(env)
+    env.logger.info("WS OPEN")
+    env['subscription'] = env.channel.subscribe { |m| env.stream_send(m) }
+  end
+
+  def on_message(env, msg)
+    env.logger.info("WS MESSAGE: #{msg}")
+    env.channel << msg
+  end
+
+  def on_close(env)
+    env.logger.info("WS CLOSED")
+    env.channel.unsubscribe(env['subscription'])
+  end
+
+  def on_error(env, error)
+    env.logger.error error
+  end
+
+  def response(env)
+    super(env) if env['REQUEST_PATH'] == '/ws'
+  end
+end
