b261ecaf3e7a5cc39d35bdcc4cc0e160b9bc49ec
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2012-08-18 09:49:36 -0700

    Added more test code for curtain testing

--- public/js/test.js
+++ public/js/test.js
@@ -1,6 +1,7 @@
 function update() {
   var index = Math.random() < 0.5 ? 0 : 1;
   var newData = racers.slice(0);
+  var interval = null;
 
   newData[index].score += parseInt(Math.random() * 5);
   newData[index].score = Array.min([maxValue, newData[index].score]);
@@ -21,6 +22,39 @@ function next() {
   }
 }
 
-var interval = setInterval(function () {
-  next();
-}, 50);
+function runTest() {
+  interval = setInterval(function () {
+    next();
+  }, 50);
+}
+
+function testSetRegistry() {
+    handleMessage(TEST_UPDATE_MESSAGE);
+}
+
+var TEST_UPDATE_MESSAGE = {
+  "event": "Update",
+  "registry": {
+    "matches": [
+      {
+        "team_names": [
+          "Team2",
+          "Team1"
+        ],
+        "scores": [
+          0,
+          0
+        ],
+        "seconds_left": null
+      }
+    ],
+    "teams": [
+      {
+        "name": "Team1"
+      },
+      {
+        "name": "Team2"
+      }
+    ]
+  }
+}
